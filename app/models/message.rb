778028d86d58c62ca92ae061ec8b581cc4380cd5
Kyle VanDeRiet <kylevdr@gmail.com>
2017-11-19 16:07:35 -0700

    Create channel and speak method

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,11 @@ class Message < ApplicationRecord
   belongs_to :chatroom
   validates :content, presence: true
   validates :author, presence: true
+  after_create :broadcast
+
+  private
+
+  def broadcast
+    ActionCable.server.broadcast('chat', as_json.merge(action: 'CreateMessage'))
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :messages, only: [:index, :create, :show]
-  resources :chatrooms, only: [:index, :create, :destroy]
+  mount ActionCable.server => '/cable'
+
+  scope 'api' do
+    resources :messages, only: [:index, :create, :show]
+    resources :chatrooms, only: [:index, :create, :destroy]
+  end
+  
 end
